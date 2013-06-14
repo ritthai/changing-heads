87e7e24f29d00f029cad662774bf8c0d5212b9bd
Ritchie Thai <RitchieThai@RitchieThai.com>
2013-06-14 05:58:46 -0300

    Moved the config to its own file

--- js/main.js
+++ js/main.js
@@ -5,20 +5,7 @@ See the file license.txt for copying permission.
 */
 
 (function () {
-	var START_SCENE_NAME = 'teaShop';
-	var BACKGROUND_DIRECTORY = 'img/backgrounds/';
-
-	adventure.getConfiguration = function () {
-		var configuration = {
-				startSceneName: START_SCENE_NAME,
-				backgroundDirectory: BACKGROUND_DIRECTORY
-			};
-		return configuration;
-	};
-
-	var init = function () {
-		var adventureEngine = adventure.getEngine().start();
-	};
-	
-	$(init);
+	$(function () {
+		adventure.getEngine().start();
+	});
 }());
