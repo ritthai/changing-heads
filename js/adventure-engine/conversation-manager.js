e5c8e5e2275a5468cc28b2052b42509e156735bb
Ritchie Thai <email excluded>
2016-09-11 19:01:55 -0700

    conversation-manager: Move the displaly oriented code to bottom below a comment

--- js/adventure-engine/conversation-manager.js
+++ js/adventure-engine/conversation-manager.js
@@ -31,7 +31,7 @@ adventure.getConversationManager = function (conversations, sceneFunctions, util
         if (line === 0 && conversation.onEnter) {
             conversation = applyConversationOnEnter(conversation);
         }
-        showConversation(conversation, line);
+        showConversation(conversation, line, advanceConversation, chooseOption);
     };
 
     var applyConversationOnEnter = function (conversation) {
@@ -48,38 +48,6 @@ adventure.getConversationManager = function (conversations, sceneFunctions, util
         return overrideConversation;
     };
 
-    var showConversation = function (conversation, line) {
-        soundManager.playClickSound();
-        conversationDisplayer.clearDialog();
-        if (conversation.dialog) {
-            writeDialog(conversation.dialog, line);
-        }
-        showSpeechBubbleLinks(conversation, line);
-    };
-
-    var showSpeechBubbleLinks = function (conversation, line) {
-        if (isLastLineOfDialog(conversation.dialog, line)) {
-            showOptions(conversation.options);
-        } else {
-            showAdvanceConversationLink(conversation, line);
-        }
-    };
-
-    var isLastLineOfDialog = function (dialog, line) {
-        return line + 1 === dialog.length / 2;
-    };
-
-    var showOptions = function (options) {
-        if (!options) { options = [{"description": "Continue"}]; }
-        options.forEach(showOption);
-        conversationDisplayer.writeOptionLn(''); // TODO: This is hacky. Use actual styling instead of adding an empty option
-    };
-
-    var showOption = function (option, i) {
-        var description = option.description || option.dialog[1];
-        conversationDisplayer.printOptionLink(description, i, function () { chooseOption(option); });
-    };
-
     var chooseOption = function (option) {
         if (option.onEnter) { option.onEnter(); }
         if (!option.next) {
@@ -90,7 +58,50 @@ adventure.getConversationManager = function (conversations, sceneFunctions, util
         advanceConversation(conversation, 0);
     };
 
-    var showAdvanceConversationLink = function (conversation, line) {
+    var endConversation = function () {
+        // NOTE: This is tricky. We cannot end the conversation immediately,
+        // because this gets evaluated before the click handler. If the click handler sees
+        // that we are not in a conversation, the character will interact with the clicked
+        // spot, even though the click was actually meant to end the conversation.
+        conversationDisplayer.hideDialogBox(function () {
+            isInConversation = false;
+        });
+    };
+
+    // TODO: Split this out into another module
+    // Everything below here is display code
+
+    var showConversation = function (conversation, line, advanceConversation, chooseOption) {
+        soundManager.playClickSound();
+        conversationDisplayer.clearDialog();
+        if (conversation.dialog) {
+            writeDialog(conversation.dialog, line);
+        }
+        if (isLastLineOfDialog(conversation.dialog, line)) {
+            showOptions(conversation.options, chooseOption);
+        } else {
+            showAdvanceConversationLink(conversation, line, advanceConversation);
+        }
+    };
+
+    var isLastLineOfDialog = function (dialog, line) {
+        return line + 1 === dialog.length / 2;
+    };
+
+    var showOptions = function (options, chooseOption) {
+        if (!options) { options = [{"description": "Continue"}]; }
+        options.forEach(function (x, i) {
+            showOption(x, i, chooseOption);
+        });
+        conversationDisplayer.writeOptionLn(''); // TODO: This is hacky. Use actual styling instead of adding an empty option
+    };
+
+    var showOption = function (option, i, chooseOption) {
+        var description = option.description || option.dialog[1];
+        conversationDisplayer.printOptionLink(description, i, function () { chooseOption(option); });
+    };
+
+    var showAdvanceConversationLink = function (conversation, line, advanceConversation) {
         conversationDisplayer.printProceedConversationLink('Next', function () {
             advanceConversation(conversation, line + 1);
         });
@@ -108,16 +119,6 @@ adventure.getConversationManager = function (conversations, sceneFunctions, util
         } else {
             writeDialogLn(speaker + ': ' + speech);
         }
-    };
-
-    var endConversation = function () {
-        // NOTE: This is tricky. We cannot end the conversation immediately,
-        // because this gets evaluated before the click handler. If the click handler sees
-        // that we are not in a conversation, the character will interact with the clicked
-        // spot, even though the click was actually meant to end the conversation.
-        conversationDisplayer.hideDialogBox(function () {
-            isInConversation = false;
-        });
     };
 
     return object;
